77d72005dc5296f45e7a12bcaf73a8f4e83c94e3
Chris Huber <chubes@chubes.net>
2026-04-28 10:08:05 -0400

    feat(content): bundle block format bridge substrate

--- tests/bootstrap-unit.php
+++ tests/bootstrap-unit.php
@@ -12,4 +12,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', '/tmp/' );
 }
 
+if ( ! function_exists( 'did_action' ) ) {
+	function did_action( $hook = '' ) {
+		return 0;
+	}
+}
+
+if ( ! function_exists( 'doing_action' ) ) {
+	function doing_action( $hook = '' ) {
+		return false;
+	}
+}
+
+if ( ! function_exists( 'add_action' ) ) {
+	function add_action( ...$args ) {
+		// no-op
+	}
+}
+
 require_once __DIR__ . '/../vendor/autoload.php';
