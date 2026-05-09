ba12b33ca7cb9e7be6836e1096998a6ef528ff63
Chris Huber <chubes@chubes.net>
2026-05-09 13:29:38 -0400

    test: align caller context host assertions

--- tests/Unit/Auth/CallerContextTest.php
+++ tests/Unit/Auth/CallerContextTest.php
@@ -89,9 +89,15 @@ class CallerContextTest extends WP_UnitTestCase {
 	}
 
 	public function test_is_cross_site_requires_remote_host(): void {
-		$local = new \WP_Agent_Caller_Context( 'some-agent', 0, \WP_Agent_Caller_Context::SELF_HOST, 2, 'cid' );
+		$local = \WP_Agent_Caller_Context::top_of_chain( 'cid' );
 		$this->assertFalse( $local->is_cross_site() );
 
+		$this->expectException( \InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'caller_host chained context (chain_depth > 0) must specify a remote caller host' );
+		new \WP_Agent_Caller_Context( 'some-agent', 0, \WP_Agent_Caller_Context::SELF_HOST, 2, 'cid' );
+	}
+
+	public function test_is_cross_site_accepts_remote_host(): void {
 		$remote = new \WP_Agent_Caller_Context( 'some-agent', 0, 'https://chubes.net', 1, 'cid' );
 		$this->assertTrue( $remote->is_cross_site() );
 	}
